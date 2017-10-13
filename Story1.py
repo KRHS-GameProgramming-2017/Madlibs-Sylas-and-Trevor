b3f09632ce9f2c8220ae05d37cb6849449575136
ClutchGasAndGo <sylas.regnier@kearsarge.org>
2017-10-13 13:14:07 -0400

    start

---
+++ Story1.py
@@ -0,0 +1,11 @@
+from Getter import *
+
+def story1(debug= False):
+	if debug: print "--In story1 function--"
+	
+	friend1 = getWord("A Name: ". debug)
+	
+	out = ""
+	out += "One day me and my friend, " + friend1
+	
+	return out
