b33cc5e5666a18c5cd77ac85d901cdcbf222a7a2
trevormcconnell <trevor.mcconnell@kearsarge.org>
2017-10-19 13:45:36 -0400

    story 2

--- Story2.py
+++ Story2.py
@@ -1,11 +1,13 @@
 from Getter import *
 
-def story1(debug= False):
+def story2(debug= False):
 	if debug: print "--In story1 function--"
 	
 	friend1 = getWord("A Name: ". debug)
-\
+    body of water = getWord(A body of water: ". debug)
+    object = getWord (an object): ". debug    
 	out = ""
 	out += "One day me and my friend, " + friend1
-	
+	out += "we were swimming in a, " + body of water1
+    out += "we swam into a, " + object1
 	return out
