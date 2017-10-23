d376d49ff6ca6878ec11bbf95f696a3338bd7ec2
ClutchGasAndGo <sylas.regnier@kearsarge.org>
2017-10-23 13:49:41 -0400

    brehbrrehhhhhhhh

--- Story1.py
+++ Story1.py
@@ -12,7 +12,8 @@ def story1(debug= False):
     out += ", went for a walk. We found a " + item1
     out += ". " + friend1 + "said it was very " + description1
 	out += "Wow it's sooo big, isn't it? " 
-    
-    
+    out += + friend2 + "Where do you think it came from?"
+    out += "I don't know, I think it's a" + item2    
+    out += "It could be, but that wouldn't explain the" 
     
 	return out 
